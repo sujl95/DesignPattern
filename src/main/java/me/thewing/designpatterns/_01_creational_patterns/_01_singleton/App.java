65ccb8d27f7a79fab733482010a04cbc0e0b4ddd
sujl95 <sujl95@naver.com>
2022-02-15 11:25:52 +0900

    싱글톤 패턴 1부 - 싱글톤 패턴을 가장 단순히 구현하는 방법

---
+++ src/main/java/me/thewing/designpatterns/_01_creational_patterns/_01_singleton/App.java
@@ -0,0 +1,9 @@
+package me.thewing.designpatterns._01_creational_patterns._01_singleton;
+
+public class App {
+
+    public static void main(String[] args) {
+        Settings settings = Settings.getInstance();
+    }
+
+}
---
+++ src/main/java/me/thewing/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
@@ -0,0 +1,16 @@
+package me.thewing.designpatterns._01_creational_patterns._01_singleton;
+
+public class Settings {
+
+  private static Settings instance;
+
+  private Settings() {}
+
+  public static Settings getInstance() {
+    if (instance == null) {
+      instance = new Settings();
+    }
+    return instance;
+  }
+
+}
