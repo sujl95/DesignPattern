b7093737f878ea24d0f1e38035963cb4fefbfd6c
sujl95 <sujl95@naver.com>
2022-03-02 14:16:02 +0900

    싱글톤 패턴 2부 - 멀티 쓰레드 환경에서 안전하게 구현하는 방법 , double checked locking

--- src/main/java/me/thewing/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
+++ src/main/java/me/thewing/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
@@ -2,12 +2,19 @@ package me.thewing.designpatterns._01_creational_patterns._01_singleton;
 
 public class Settings {
 
-  private static Settings INSTANCE = new Settings();
+  private static Settings instance;
 
   private Settings() {}
 
   public static Settings getInstance() {
-    return INSTANCE;
+    if (instance == null) {
+      synchronized (Settings.class) {
+        if (instance == null) {
+          instance = new Settings();
+        }
+      }
+    }
+    return instance;
   }
 
 }
