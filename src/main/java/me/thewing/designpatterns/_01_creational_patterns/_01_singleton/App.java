233fe735b678eea995eafcaba1177447e6bf8c1d
sujl95 <sujl95@naver.com>
2022-03-02 14:50:04 +0900

    싱글톤 패턴 4부 - 안전하고 단순하게 구현하는 방법 , Enum 사용

--- src/main/java/me/thewing/designpatterns/_01_creational_patterns/_01_singleton/App.java
+++ src/main/java/me/thewing/designpatterns/_01_creational_patterns/_01_singleton/App.java
@@ -1,18 +1,18 @@
 package me.thewing.designpatterns._01_creational_patterns._01_singleton;
 
-import java.io.*;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class App {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Settings settings = Settings.getInstance();
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Settings settings = Settings.INSTANCE;
         Settings settings1 = null;
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("setting.obj"))) {
-            out.writeObject(settings);
-        }
-
-        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
-            settings1 = (Settings) in.readObject();
+        Constructor<?>[] constructors = Settings.class.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            constructor.setAccessible(true);
+            settings1 = (Settings) constructor.newInstance("INSTANCE");
         }
 
         System.out.println(settings == settings1);
--- src/main/java/me/thewing/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
+++ src/main/java/me/thewing/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
@@ -1,21 +1,5 @@
 package me.thewing.designpatterns._01_creational_patterns._01_singleton;
 
-import java.io.Serializable;
-
-public class Settings implements Serializable {
-
-  private Settings() {}
-
-  private static class SettingHolder {
-    private static final Settings INSTANCE = new Settings();
-  }
-
-  public static Settings getInstance() {
-    return SettingHolder.INSTANCE;
-  }
-
-  protected Object readResolve() {
-    return getInstance();
-  }
-
+public enum Settings {
+  INSTANCE;
 }
