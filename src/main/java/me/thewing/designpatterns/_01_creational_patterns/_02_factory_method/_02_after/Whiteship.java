f85afc70ee7e84e8e396a332a60e18e9a81b3c93
sujl95 <sujl95@naver.com>
2022-03-02 22:57:21 +0900

    팩토리 메서드

---
+++ src/main/java/me/thewing/designpatterns/_01_creational_patterns/_02_factory_method/_02_after/Whiteship.java
@@ -0,0 +1,10 @@
+package me.thewing.designpatterns._01_creational_patterns._02_factory_method._02_after;
+
+public class Whiteship extends Ship {
+
+    public Whiteship() {
+        setName("whiteship");
+        setLogo("\uD83D\uDEE5");
+        setColor("white");
+    }
+}
