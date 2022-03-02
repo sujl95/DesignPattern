d311aa8a47e61020fc89018d0c7dc6f0a8874d36
sujl95 <sujl95@naver.com>
2022-03-02 14:58:44 +0900

    싱글톤 패턴 5부 - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/me/thewing/designpatterns/_01_creational_patterns/_01_singleton/RuntimeExample.java
@@ -0,0 +1,10 @@
+package me.thewing.designpatterns._01_creational_patterns._01_singleton;
+
+public class RuntimeExample {
+
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println(runtime.maxMemory());
+        System.out.println(runtime.freeMemory());
+    }
+}
