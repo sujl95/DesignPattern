612d4bd9a57cd00e323b8279d0552406a4830237
sujl95 <sujl95@naver.com>
2022-03-02 22:56:34 +0900

    팩토리 메서드

--- src/main/java/me/thewing/designpatterns/_02_factorymethod/_02_after/BlackshipFactory.java
+++
@@ -1,8 +0,0 @@
-package me.thewing.designpatterns._02_factorymethod._02_after;
-
-public class BlackshipFactory implements ShipFactory {
-    @Override
-    public Ship createShip() {
-        return new Blackship();
-    }
-}
--- src/main/java/me/thewing/designpatterns/_02_factorymethod/_02_after/WhiteshipFactory.java
+++
@@ -1,9 +0,0 @@
-package me.thewing.designpatterns._02_factorymethod._02_after;
-
-public class WhiteshipFactory implements ShipFactory{
-
-    @Override
-    public Ship createShip() {
-        return new Whiteship();
-    }
-}
