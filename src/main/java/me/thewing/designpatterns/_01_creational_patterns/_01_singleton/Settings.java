46413f55e41a7289f9356187d904050248ad03f6
sujl95 <sujl95@naver.com>
2022-03-02 14:20:26 +0900

    싱글톤 패턴 2부 - 멀티 쓰레드 환경에서 안전하게 구현하는 방법 , static inner 클래스

--- src/main/java/me/thewing/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
+++ src/main/java/me/thewing/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
@@ -2,19 +2,14 @@ package me.thewing.designpatterns._01_creational_patterns._01_singleton;
 
 public class Settings {
 
-  private static volatile Settings instance;
-
   private Settings() {}
 
+  private static class SettingHolder {
+    private static final Settings INSTANCE = new Settings();
+  }
+
   public static Settings getInstance() {
-    if (instance == null) {
-      synchronized (Settings.class) {
-        if (instance == null) {
-          instance = new Settings();
-        }
-      }
-    }
-    return instance;
+    return SettingHolder.INSTANCE;
   }
 
 }
