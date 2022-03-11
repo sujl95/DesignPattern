e359b59cc2f78a80370aaa8601855d6e170e67c5
sujl95 <sujl95@naver.com>
2022-03-11 15:54:46 +0900

    인터프리터 패턴 2부 - 패턴 적용

---
+++ src/main/java/me/thewing/designpatterns/_03_behavioral_patterns/_15_interpreter/_02_after/PostfixExpression.java
@@ -0,0 +1,9 @@
+package me.thewing.designpatterns._03_behavioral_patterns._15_interpreter._02_after;
+
+import java.util.Map;
+
+public interface PostfixExpression {
+
+    int interpret(Map<Character, Integer> context);
+
+}
